892e3be5e36fa577bfc3a8fabeb532140fa8e76d
Johnson Zhang <jzhang@Johnsons-MacBook-Pro.local>
2021-11-06 16:08:10 -0400

    initial form

--- App.js
+++ App.js
@@ -6,12 +6,25 @@ export default function App() {
     <View style={styles.container}>
       <form>
         <label>
-          What disabilities do you experience?:
+          What disabilities are you experiencing?:
         <select>
-          <option value="grapefruit">Grapefruit</option>
-          <option value="lime">Lime</option>
-          <option selected value="coconut">Coconut</option>
-          <option value="mango">Mango</option>
+          <option value="" disabled selected></option>
+          <option value="Acquired brain injury">Acquired brain injury</option>
+          <option value="ALS/Lou Gehrig’s">ALS/Lou Gehrig’s</option>
+          <option value="Amputation">Amputation</option>
+          <option value="Anxiety disorders">Anxiety disorders</option>
+          <option value="Arthritis">Arthritis</option>
+          <option value="Autism Spectrum Disorders">Autism Spectrum Disorders</option>
+          <option value="Cerebral Palsy">Cerebral Palsy</option>
+          <option value="Chronic Pain">Chronic Pain</option>
+          <option value="Deafness or Hard of Hearing">CDeafness or Hard of Hearing</option>
+          <option value="Dementia">Dementia</option>
+          <option value="Epilepsy">Epilepsy</option>
+          <option value="Multiple Sclerosis">Multiple Sclerosis</option>
+          <option value="Parkinson’s">Parkinson’s</option>
+          <option value="Stuttering">Stuttering</option>
+          <option value="Tourrette’s Syndrome">Tourrette’s Syndrome</option>
+          <option value="Visual Impairments">Visual Impairments</option>
         </select>
         </label>
       </form>
